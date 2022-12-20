263cca62795ec31df93ab395196b146ddbc88523
Tanishq Agarwal <tanishqoct11@gmail.com>
2022-12-20 21:25:03 +0530

    Added UI Background to App!!

--- tedxjuet/src/Components/Home/Home.js
+++ tedxjuet/src/Components/Home/Home.js
@@ -4,9 +4,15 @@ import "../Home/home.module.css";
 function Home(){
 return(
 <div id="root">
-   <div>
-      
-   </div>
+<div class="header">
+        <div class="navbar">
+          <div>Home</div>
+          <div>About</div>
+          <div>Meet the Team</div>
+          <div></div>
+          
+        </div>
+      </div>
 </div>
 );
 }
