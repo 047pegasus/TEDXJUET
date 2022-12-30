0eb0073024f7222b9a37843fdf48f56b8690cc94
Tanishq Agarwal <tanishqoct11@gmail.com>
2022-12-30 13:09:46 +0530

    Added Speakers Component structure✅

--- tedxjuet/src/Components/Speakers/Speakers.js
+++ tedxjuet/src/Components/Speakers/Speakers.js
@@ -16,13 +16,7 @@ import "slick-carousel/slick/slick-theme.css";
 function Speakers(){
   const navigate = useNavigate();
   navigate("/speakers");
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
+
 return(
   <div id="root">
     <div class="navbar">
@@ -36,26 +30,7 @@ return(
     </div>
     
     <div className={styles.maincontainer}>
-    <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-        </Slider>    
+      
     </div>
 
     <div class="footer">
