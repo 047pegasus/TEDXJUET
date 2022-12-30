9f276e2921294b7e2225d554b6e0d12d6c15030e
Tanishq Agarwal <tanishqoct11@gmail.com>
2022-12-30 16:45:53 +0530

    Added Speakers Page structure✅

--- tedxjuet/src/Components/Speakers/Speakers.js
+++ tedxjuet/src/Components/Speakers/Speakers.js
@@ -57,19 +57,89 @@ Elizabeth Finkle is an experienced worksite wellbeing practitioner with 16 years
            </div>
           </div>
           <div className={styles.carousecontent}>
-            <h3>2</h3>
+          <div className={styles.card}>
+       <div className={styles.content}>
+           <div className={styles.front}>
+              <img className={styles.speakpic} src="" alt="Pic"></img>
+               <h3 className={styles.title}>ISHA SEHGAL</h3>
+               <p className={styles.subtitle}>Organizer, TEDx JUET</p>
+         </div>
+           <div className={styles.back}>
+               <p className={styles.description}>
+               “Normalizing all emotions in the workplace can deepen connectivity, inclusivity and wellbeing on our team environments and empower individuals to thrive. In our current climate, we are faced by complex uncertainty every day. Bringing our full humanity to work is imperative to our mental and physical health. Emotional agility is a superpower, and it starts with the basic step of noticing. When we practice selective vulnerability coupled with a path to move through our emotions; innovation, creativity and productivity soars!
+Elizabeth Finkle is an experienced worksite wellbeing practitioner with 16 years demonstrated history in designing environments to promote lasting health and wellbeing. Beth’s purpose is to help organizations create the conditions for people to thrive, work in effective ways and feel joy. Throughout her career, Beth has worked in a variety of employer settings including hospitals, health insurance companies, national sports teams, manufacturing sites and most recently higher education at the University of Delaware. Her superpower is people, with a flair for simplicity and she loves working on complex people scenarios. She is a demonstrated leader in organizational health initiatives, employee experience design, program planning and professional development creation.”
+               </p>
+           </div>
+           </div>
+           </div>
           </div>
           <div className={styles.carousecontent}>
-            <h3>3</h3>
+          <div className={styles.card}>
+       <div className={styles.content}>
+           <div className={styles.front}>
+              <img className={styles.speakpic} src="" alt="Pic"></img>
+               <h3 className={styles.title}>ISHA SEHGAL</h3>
+               <p className={styles.subtitle}>Organizer, TEDx JUET</p>
+         </div>
+           <div className={styles.back}>
+               <p className={styles.description}>
+               “Normalizing all emotions in the workplace can deepen connectivity, inclusivity and wellbeing on our team environments and empower individuals to thrive. In our current climate, we are faced by complex uncertainty every day. Bringing our full humanity to work is imperative to our mental and physical health. Emotional agility is a superpower, and it starts with the basic step of noticing. When we practice selective vulnerability coupled with a path to move through our emotions; innovation, creativity and productivity soars!
+Elizabeth Finkle is an experienced worksite wellbeing practitioner with 16 years demonstrated history in designing environments to promote lasting health and wellbeing. Beth’s purpose is to help organizations create the conditions for people to thrive, work in effective ways and feel joy. Throughout her career, Beth has worked in a variety of employer settings including hospitals, health insurance companies, national sports teams, manufacturing sites and most recently higher education at the University of Delaware. Her superpower is people, with a flair for simplicity and she loves working on complex people scenarios. She is a demonstrated leader in organizational health initiatives, employee experience design, program planning and professional development creation.”
+               </p>
+           </div>
+           </div>
+           </div>
           </div>
           <div className={styles.carousecontent}>
-            <h3>4</h3>
+          <div className={styles.card}>
+       <div className={styles.content}>
+           <div className={styles.front}>
+              <img className={styles.speakpic} src="" alt="Pic"></img>
+               <h3 className={styles.title}>ISHA SEHGAL</h3>
+               <p className={styles.subtitle}>Organizer, TEDx JUET</p>
+         </div>
+           <div className={styles.back}>
+               <p className={styles.description}>
+               “Normalizing all emotions in the workplace can deepen connectivity, inclusivity and wellbeing on our team environments and empower individuals to thrive. In our current climate, we are faced by complex uncertainty every day. Bringing our full humanity to work is imperative to our mental and physical health. Emotional agility is a superpower, and it starts with the basic step of noticing. When we practice selective vulnerability coupled with a path to move through our emotions; innovation, creativity and productivity soars!
+Elizabeth Finkle is an experienced worksite wellbeing practitioner with 16 years demonstrated history in designing environments to promote lasting health and wellbeing. Beth’s purpose is to help organizations create the conditions for people to thrive, work in effective ways and feel joy. Throughout her career, Beth has worked in a variety of employer settings including hospitals, health insurance companies, national sports teams, manufacturing sites and most recently higher education at the University of Delaware. Her superpower is people, with a flair for simplicity and she loves working on complex people scenarios. She is a demonstrated leader in organizational health initiatives, employee experience design, program planning and professional development creation.”
+               </p>
+           </div>
+           </div>
+           </div>
           </div>
           <div className={styles.carousecontent}>
-            <h3>5</h3>
+          <div className={styles.card}>
+       <div className={styles.content}>
+           <div className={styles.front}>
+              <img className={styles.speakpic} src="" alt="Pic"></img>
+               <h3 className={styles.title}>ISHA SEHGAL</h3>
+               <p className={styles.subtitle}>Organizer, TEDx JUET</p>
+         </div>
+           <div className={styles.back}>
+               <p className={styles.description}>
+               “Normalizing all emotions in the workplace can deepen connectivity, inclusivity and wellbeing on our team environments and empower individuals to thrive. In our current climate, we are faced by complex uncertainty every day. Bringing our full humanity to work is imperative to our mental and physical health. Emotional agility is a superpower, and it starts with the basic step of noticing. When we practice selective vulnerability coupled with a path to move through our emotions; innovation, creativity and productivity soars!
+Elizabeth Finkle is an experienced worksite wellbeing practitioner with 16 years demonstrated history in designing environments to promote lasting health and wellbeing. Beth’s purpose is to help organizations create the conditions for people to thrive, work in effective ways and feel joy. Throughout her career, Beth has worked in a variety of employer settings including hospitals, health insurance companies, national sports teams, manufacturing sites and most recently higher education at the University of Delaware. Her superpower is people, with a flair for simplicity and she loves working on complex people scenarios. She is a demonstrated leader in organizational health initiatives, employee experience design, program planning and professional development creation.”
+               </p>
+           </div>
+           </div>
+           </div>
           </div>
           <div className={styles.carousecontent}>
-            <h3>6</h3>
+          <div className={styles.card}>
+       <div className={styles.content}>
+           <div className={styles.front}>
+              <img className={styles.speakpic} src="" alt="Pic"></img>
+               <h3 className={styles.title}>ISHA SEHGAL</h3>
+               <p className={styles.subtitle}>Organizer, TEDx JUET</p>
+         </div>
+           <div className={styles.back}>
+               <p className={styles.description}>
+               “Normalizing all emotions in the workplace can deepen connectivity, inclusivity and wellbeing on our team environments and empower individuals to thrive. In our current climate, we are faced by complex uncertainty every day. Bringing our full humanity to work is imperative to our mental and physical health. Emotional agility is a superpower, and it starts with the basic step of noticing. When we practice selective vulnerability coupled with a path to move through our emotions; innovation, creativity and productivity soars!
+Elizabeth Finkle is an experienced worksite wellbeing practitioner with 16 years demonstrated history in designing environments to promote lasting health and wellbeing. Beth’s purpose is to help organizations create the conditions for people to thrive, work in effective ways and feel joy. Throughout her career, Beth has worked in a variety of employer settings including hospitals, health insurance companies, national sports teams, manufacturing sites and most recently higher education at the University of Delaware. Her superpower is people, with a flair for simplicity and she loves working on complex people scenarios. She is a demonstrated leader in organizational health initiatives, employee experience design, program planning and professional development creation.”
+               </p>
+           </div>
+           </div>
+           </div>
           </div>
         </Slider>
         </div>
