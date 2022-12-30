de6e2f35c7980f7fc70c7b8d669bdd1436d8437f
Tanishq Agarwal <tanishqoct11@gmail.com>
2022-12-30 17:11:22 +0530

    Speakers Page Populated✅

--- tedxjuet/src/Components/Speakers/Speakers.js
+++ tedxjuet/src/Components/Speakers/Speakers.js
@@ -44,13 +44,12 @@ return(
        <div className={styles.content}>
            <div className={styles.front}>
               <img className={styles.speakpic} src="" alt="Pic"></img>
-               <h3 className={styles.title}>ISHA SEHGAL</h3>
-               <p className={styles.subtitle}>Organizer, TEDx JUET</p>
+               <h3 className={styles.title}>JAYA SHARMA(KISHORI)</h3>
+               <p className={styles.subtitle}>World Peace, 2023 Speaker, TEDx JUET</p>
          </div>
            <div className={styles.back}>
                <p className={styles.description}>
-               “Normalizing all emotions in the workplace can deepen connectivity, inclusivity and wellbeing on our team environments and empower individuals to thrive. In our current climate, we are faced by complex uncertainty every day. Bringing our full humanity to work is imperative to our mental and physical health. Emotional agility is a superpower, and it starts with the basic step of noticing. When we practice selective vulnerability coupled with a path to move through our emotions; innovation, creativity and productivity soars!
-Elizabeth Finkle is an experienced worksite wellbeing practitioner with 16 years demonstrated history in designing environments to promote lasting health and wellbeing. Beth’s purpose is to help organizations create the conditions for people to thrive, work in effective ways and feel joy. Throughout her career, Beth has worked in a variety of employer settings including hospitals, health insurance companies, national sports teams, manufacturing sites and most recently higher education at the University of Delaware. Her superpower is people, with a flair for simplicity and she loves working on complex people scenarios. She is a demonstrated leader in organizational health initiatives, employee experience design, program planning and professional development creation.”
+               “Jaya Kishori is an Indian musician and spiritual speaker who is famous for her inspiring talks and religious albums. She did her schooling from Shri Shikshayatan College and Mahadevi Birla World Academy in Kolkata. She has done B.Com via open schooling. Jaya Kishori is popular for life management tips and motivational speeches, she speaks periodically through seminars and webinars on different topics related to life, she started her spiritual journey at the tender age of seven. Ever since her childhood awakening, she developed a staunch belief in fate and believed that nothing is ever planned - it's all destined.She has been celebrated as the 'Youth Spiritual Icon' by Fame India Magazine and has been the recipient of the Women Era award for her valuable contribution towards the nation. Recently she has been awarded with "Iconic Woman Motivational Speaker of the year 2021" by Iconic Gold Awards 2021 and " Best Spiritual Influencer" by Lokmat - Digital Influencer Awards 2021. She has also been awarded the " Best Motivational Speaker 2021 " by the World Digital detox Day. She also received the Most Inspiring Woman of the Year (Spiritual) at Global Excellence Awards (GEA) in May 2022.”
                </p>
            </div>
            </div>
@@ -61,13 +60,12 @@ Elizabeth Finkle is an experienced worksite wellbeing practitioner with 16 years
        <div className={styles.content}>
            <div className={styles.front}>
               <img className={styles.speakpic} src="" alt="Pic"></img>
-               <h3 className={styles.title}>ISHA SEHGAL</h3>
-               <p className={styles.subtitle}>Organizer, TEDx JUET</p>
+               <h3 className={styles.title}>SUNITA NARAIN</h3>
+               <p className={styles.subtitle}>Nature, 2023 Speaker, TEDx JUET</p>
          </div>
            <div className={styles.back}>
                <p className={styles.description}>
-               “Normalizing all emotions in the workplace can deepen connectivity, inclusivity and wellbeing on our team environments and empower individuals to thrive. In our current climate, we are faced by complex uncertainty every day. Bringing our full humanity to work is imperative to our mental and physical health. Emotional agility is a superpower, and it starts with the basic step of noticing. When we practice selective vulnerability coupled with a path to move through our emotions; innovation, creativity and productivity soars!
-Elizabeth Finkle is an experienced worksite wellbeing practitioner with 16 years demonstrated history in designing environments to promote lasting health and wellbeing. Beth’s purpose is to help organizations create the conditions for people to thrive, work in effective ways and feel joy. Throughout her career, Beth has worked in a variety of employer settings including hospitals, health insurance companies, national sports teams, manufacturing sites and most recently higher education at the University of Delaware. Her superpower is people, with a flair for simplicity and she loves working on complex people scenarios. She is a demonstrated leader in organizational health initiatives, employee experience design, program planning and professional development creation.”
+               “Sunita Narain has been with the Centre for Science and Environment (CSE) since 1982. Graduated From the University of Delhi(1983),Doctors of Science(Honorary), Cranfield University, UK, D.Sc. Degree, University of Calcutta, she is a writer and environmentalist, who uses knowledge for change. In 2005 she was awarded the Padma Shri by the Indian government. She has also received the World Water Prize for work on rainwater harvesting and for its policy influence in building paradigms for community-based water management. Narain began her work in the early 1980s, as a co-researcher with Anil Agarwal, an eminent and committed environmentalist who gave the country its environmental concern and message. In 1985, she co-edited the State of India's Environment report, which built an understanding in the country of why India is so important for the poor.”
                </p>
            </div>
            </div>
@@ -78,13 +76,13 @@ Elizabeth Finkle is an experienced worksite wellbeing practitioner with 16 years
        <div className={styles.content}>
            <div className={styles.front}>
               <img className={styles.speakpic} src="" alt="Pic"></img>
-               <h3 className={styles.title}>ISHA SEHGAL</h3>
-               <p className={styles.subtitle}>Organizer, TEDx JUET</p>
+               <h3 className={styles.title}>LAXMI NARAYAN TRIPATHI</h3>
+               <p className={styles.subtitle}>Transgender Rights, 2023 Speaker, TEDx JUET</p>
          </div>
            <div className={styles.back}>
                <p className={styles.description}>
-               “Normalizing all emotions in the workplace can deepen connectivity, inclusivity and wellbeing on our team environments and empower individuals to thrive. In our current climate, we are faced by complex uncertainty every day. Bringing our full humanity to work is imperative to our mental and physical health. Emotional agility is a superpower, and it starts with the basic step of noticing. When we practice selective vulnerability coupled with a path to move through our emotions; innovation, creativity and productivity soars!
-Elizabeth Finkle is an experienced worksite wellbeing practitioner with 16 years demonstrated history in designing environments to promote lasting health and wellbeing. Beth’s purpose is to help organizations create the conditions for people to thrive, work in effective ways and feel joy. Throughout her career, Beth has worked in a variety of employer settings including hospitals, health insurance companies, national sports teams, manufacturing sites and most recently higher education at the University of Delaware. Her superpower is people, with a flair for simplicity and she loves working on complex people scenarios. She is a demonstrated leader in organizational health initiatives, employee experience design, program planning and professional development creation.”
+               “Laxmi Narayan Tripathi is a transgender rights activist, Bollywood actress, Bharatanatyam dancer, choreographer, and motivational speaker in Mumbai, India. She is also the Acharya Mahamandaleshwar of kinnar akhada. She was born in MaltiBai Hospital on 13th Dec 1978 in Thane. She is the first transgender person to represent Asia Pacific in the UN in 2008. At the assembly, she spoke of the plight of sexual minorities. "People should be more humanlike." They should respect us as humans and consider our rights as transgender people," she said. She was a contestant on the popular reality show Bigg Boss in 2011. Her efforts helped the first transgender team scale a Himalayan peak (Friendship Peak) in 2020.Indian
+transgender activist Laxmi Narayan Tripathi is now keen to uplift the community and provide value to society by offering professional services and manpower through her startup, Kineer (a packaged drinking brand).”
                </p>
            </div>
            </div>
@@ -95,47 +93,15 @@ Elizabeth Finkle is an experienced worksite wellbeing practitioner with 16 years
        <div className={styles.content}>
            <div className={styles.front}>
               <img className={styles.speakpic} src="" alt="Pic"></img>
-               <h3 className={styles.title}>ISHA SEHGAL</h3>
-               <p className={styles.subtitle}>Organizer, TEDx JUET</p>
+               <h3 className={styles.title}>ANANT LADHA</h3>
+               <p className={styles.subtitle}>FINANCE, 2023 Speaker, TEDx JUET</p>
          </div>
            <div className={styles.back}>
                <p className={styles.description}>
-               “Normalizing all emotions in the workplace can deepen connectivity, inclusivity and wellbeing on our team environments and empower individuals to thrive. In our current climate, we are faced by complex uncertainty every day. Bringing our full humanity to work is imperative to our mental and physical health. Emotional agility is a superpower, and it starts with the basic step of noticing. When we practice selective vulnerability coupled with a path to move through our emotions; innovation, creativity and productivity soars!
-Elizabeth Finkle is an experienced worksite wellbeing practitioner with 16 years demonstrated history in designing environments to promote lasting health and wellbeing. Beth’s purpose is to help organizations create the conditions for people to thrive, work in effective ways and feel joy. Throughout her career, Beth has worked in a variety of employer settings including hospitals, health insurance companies, national sports teams, manufacturing sites and most recently higher education at the University of Delaware. Her superpower is people, with a flair for simplicity and she loves working on complex people scenarios. She is a demonstrated leader in organizational health initiatives, employee experience design, program planning and professional development creation.”
-               </p>
-           </div>
-           </div>
-           </div>
-          </div>
-          <div className={styles.carousecontent}>
-          <div className={styles.card}>
-       <div className={styles.content}>
-           <div className={styles.front}>
-              <img className={styles.speakpic} src="" alt="Pic"></img>
-               <h3 className={styles.title}>ISHA SEHGAL</h3>
-               <p className={styles.subtitle}>Organizer, TEDx JUET</p>
-         </div>
-           <div className={styles.back}>
-               <p className={styles.description}>
-               “Normalizing all emotions in the workplace can deepen connectivity, inclusivity and wellbeing on our team environments and empower individuals to thrive. In our current climate, we are faced by complex uncertainty every day. Bringing our full humanity to work is imperative to our mental and physical health. Emotional agility is a superpower, and it starts with the basic step of noticing. When we practice selective vulnerability coupled with a path to move through our emotions; innovation, creativity and productivity soars!
-Elizabeth Finkle is an experienced worksite wellbeing practitioner with 16 years demonstrated history in designing environments to promote lasting health and wellbeing. Beth’s purpose is to help organizations create the conditions for people to thrive, work in effective ways and feel joy. Throughout her career, Beth has worked in a variety of employer settings including hospitals, health insurance companies, national sports teams, manufacturing sites and most recently higher education at the University of Delaware. Her superpower is people, with a flair for simplicity and she loves working on complex people scenarios. She is a demonstrated leader in organizational health initiatives, employee experience design, program planning and professional development creation.”
-               </p>
-           </div>
-           </div>
-           </div>
-          </div>
-          <div className={styles.carousecontent}>
-          <div className={styles.card}>
-       <div className={styles.content}>
-           <div className={styles.front}>
-              <img className={styles.speakpic} src="" alt="Pic"></img>
-               <h3 className={styles.title}>ISHA SEHGAL</h3>
-               <p className={styles.subtitle}>Organizer, TEDx JUET</p>
-         </div>
-           <div className={styles.back}>
-               <p className={styles.description}>
-               “Normalizing all emotions in the workplace can deepen connectivity, inclusivity and wellbeing on our team environments and empower individuals to thrive. In our current climate, we are faced by complex uncertainty every day. Bringing our full humanity to work is imperative to our mental and physical health. Emotional agility is a superpower, and it starts with the basic step of noticing. When we practice selective vulnerability coupled with a path to move through our emotions; innovation, creativity and productivity soars!
-Elizabeth Finkle is an experienced worksite wellbeing practitioner with 16 years demonstrated history in designing environments to promote lasting health and wellbeing. Beth’s purpose is to help organizations create the conditions for people to thrive, work in effective ways and feel joy. Throughout her career, Beth has worked in a variety of employer settings including hospitals, health insurance companies, national sports teams, manufacturing sites and most recently higher education at the University of Delaware. Her superpower is people, with a flair for simplicity and she loves working on complex people scenarios. She is a demonstrated leader in organizational health initiatives, employee experience design, program planning and professional development creation.”
+               “Anant Ladha is a YouTuber & Investment Creator who is best known as the founder of Invest Aaj for Kal.
+He is a distinguished Chartered Accountant, Certified Financial Planner, Chartered Financial Analyst Level 3 candidate and has conducted 300+ Financial Literacy Sessions covering more than 15000 people. He is frequently featured in all leading magazines and newspapers, be it Times, Money Life, or state-owned media house. An official CA Institute trainer and also an avid learner of new market trends.Anant Ladha complete his schooling at Modern School, Kota then he went on to attend HR College of Commerce and Economics and graduated with a Bachelor of Commerce degree in 2015.
+Ladha pursued a Bachelor of Law at Government law college National Law University, Kota in 2018.
+Anant attended The Institute Of Chartered Accounts of India for CA and received his CA degree in 2016.”
                </p>
            </div>
            </div>
